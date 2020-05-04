3a53b938ee3d0dac322dd35273b21c0c4dd1ae95
Morel Fotsing <morelt98@gmail.com>
2020-05-03 17:53:24 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Morel Fotsing on 5/3/20.
+//  Copyright © 2020 Morel Fotsing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
